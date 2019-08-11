eec00c9ab83c0b00272d14f3943daf7c04df16a4
alekseyburn <alekseyburn@gmail.com>
2019-08-11 15:33:17 +0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+
+    <my-select></my-select>
+  </div>
+</template>
+
+<script>
+  import MySelect from './MySelect.vue'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+
+    }
+  },
+  components: {
+    mySelect: MySelect
+  }
+}
+</script>
+
+<style>
+
+</style>
